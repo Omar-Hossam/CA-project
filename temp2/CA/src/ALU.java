d7415858a106dfe0699571365b95d1627784b789
Omar Kassem <omar.kassem67@gmail.com>
2013-04-22 01:28:00 +0200

    sll we srl

--- temp2/CA/src/ALU.java
+++ temp2/CA/src/ALU.java
@@ -1,10 +1,24 @@
 public class ALU {
 	public String performOperation(String data1, String data2, int fifi){
 		long temp = 0;
+		long x;
+		long y;
 		String binaryValue;
 		switch (fifi){
 			case 100000: 
 				temp = Long.parseLong(data1,2)+Long.parseLong(data2,2);
+				binaryValue = Long.toBinaryString(temp);
+				return binaryValue;
+			case 000000:
+			    x= convert_to_long(data1);
+			    y= convert_to_long(data2);
+				temp = x << y;
+				binaryValue = Long.toBinaryString(temp);
+				return binaryValue;
+			case 000010:
+				x= convert_to_long(data1);
+				y= convert_to_long(data2);
+				temp =x >>> y;
 				binaryValue = Long.toBinaryString(temp);
 				return binaryValue;
 			case 100111:
@@ -30,4 +44,17 @@ public class ALU {
 		}
 		return "";
 	}
+	public static long convert_to_long(String x){
+		long sum=0;
+		String z="";
+		long y=0;
+		for(int i=0;i<x.length();i++){
+			z=z+x.charAt(i);
+			y=Long.parseLong(z, 2);
+			sum=sum+((long) (Math.pow(2, x.length()-i-1))*y);
+			z="";
+		}
+			return sum;
+	}
+	
 }
