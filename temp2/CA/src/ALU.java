32846f9e1a9c74c1d8e8e0cac52783bb097dcc5e
Omar Kassem <omar.kassem67@gmail.com>
2013-05-07 15:48:45 +0200

    merged with master

--- temp2/CA/src/ALU.java
+++ temp2/CA/src/ALU.java
@@ -1,47 +1,98 @@
 public class ALU {
-	public String performOperation(String data1, String data2, int fifi) {
-		long temp = 0;
-		long x;
-		long y;
-		String binaryValue;
-		switch (fifi) {
-		case 100000:
-			temp = Long.parseLong(data1, 2) + Long.parseLong(data2, 2);
-			binaryValue = Long.toBinaryString(temp);
-			return binaryValue;
-		case 100111:
-			temp = (Long.parseLong(data1, 2) | Long.parseLong(data2, 2));
-			String temp2 = Long.toBinaryString(temp);
-			binaryValue = "";
-			for (int i = 0; i < temp2.length(); i++) {
-				if (temp2.charAt(i) == '1') {
-					binaryValue += "0";
-				} else {
-					binaryValue += "1";
-				}
-			}
-			return binaryValue;
-		case 100101:
-			temp = Long.parseLong(data1, 2) | Long.parseLong(data2, 2);
-			String orValue = Long.toBinaryString(temp);
-			return orValue;
-		case 100010:
-			temp = Long.parseLong(data1, 2) - Long.parseLong(data2, 2);
-			binaryValue = Long.toBinaryString(temp);
-			return binaryValue;
 
-		case 000000:
-			temp = Long.parseLong(data1, 2) << Long.parseLong(data2, 2);
-			binaryValue = Long.toBinaryString(temp);
-			return binaryValue;
+  public String performOperation(String data1, String data2, int fifi){
+    
 
-		case 000010:
-			temp = Long.parseLong(data1, 2) >>> Long.parseLong(data2, 2);
-			binaryValue = Long.toBinaryString(temp);
-			return binaryValue;
-		}
+    long temp = 0;
+    long x;
+    long y;
+    String binaryValue;
 
-		return "";
-	}
+    switch (fifi) {
+    case 100000:
+      temp = Long.parseLong(data1, 2) + Long.parseLong(data2, 2);
+      binaryValue = Long.toBinaryString(temp);
+      return binaryValue;
+    case 100111:
+      temp = (Long.parseLong(data1, 2) | Long.parseLong(data2, 2));
+      String temp2 = Long.toBinaryString(temp);
+      binaryValue = "";
+      for (int i = 0; i < temp2.length(); i++) {
+        if (temp2.charAt(i) == '1') {
+          binaryValue += "0";
+        } else {
+          binaryValue += "1";
+        }
+      }
+      return binaryValue;
+    case 100101:
+      temp = Long.parseLong(data1, 2) | Long.parseLong(data2, 2);
+      String orValue = Long.toBinaryString(temp);
+      return orValue;
+    case 100010:
+      temp = Long.parseLong(data1, 2) - Long.parseLong(data2, 2);
+      binaryValue = Long.toBinaryString(temp);
+      return binaryValue;
+
+    case 000000:
+      temp = Long.parseLong(data1, 2) << Long.parseLong(data2, 2);
+      binaryValue = Long.toBinaryString(temp);
+      return binaryValue;
+
+    case 000010:
+      temp = Long.parseLong(data1, 2) >>> Long.parseLong(data2, 2);
+      binaryValue = Long.toBinaryString(temp);
+      return binaryValue;
+    }
+
+
+    switch (fifi){
+      
+      case 100000: 
+        temp = Long.parseLong(data1,2)+Long.parseLong(data2,2);
+        binaryValue = Long.toBinaryString(temp);
+        return binaryValue;
+      
+      case 100111:
+        temp = (Long.parseLong(data1,2)|Long.parseLong(data2,2));
+        String temp2 = Long.toBinaryString(temp);
+        binaryValue = "";
+        for(int i = 0; i < temp2.length(); i++ ) {
+          if(temp2.charAt(i) == '1') {
+            binaryValue += "0";
+          } else {
+            binaryValue += "1";
+          }
+        }
+        return binaryValue;
+      
+      case 100101:
+        temp = Long.parseLong(data1,2) | Long.parseLong(data2,2);
+        String orValue = Long.toBinaryString(temp);
+        return orValue;
+      
+      case 100100:
+        temp = Long.parseLong(data1,2) & Long.parseLong(data2,2);
+        String andValue = Long.toBinaryString(temp);
+        return andValue;
+      
+      case 100010:
+        temp = Long.parseLong(data1,2) - Long.parseLong(data2,2);
+        binaryValue = Long.toBinaryString(temp);
+        return binaryValue; 
+      case 000000:
+        temp = Long.parseLong(data1, 2) << Long.parseLong(data2, 2);
+        binaryValue = Long.toBinaryString(temp);
+        return binaryValue;
+
+      case 000010:
+        temp = Long.parseLong(data1, 2) >>> Long.parseLong(data2, 2);
+        binaryValue = Long.toBinaryString(temp);
+        return binaryValue;
+    }
+    
+
+    return "";
+  }
 
 }
--- temp2/CA/src/Control.java
+++ temp2/CA/src/Control.java
@@ -1,105 +1,165 @@
 public class Control {
-	int regDst;
-	int ALUSrc;
-	int MemToReg;
-	int RegWrite;
-	int MemRead;
-	int MemWrite;
-	int branch;
-	int ALUOp1;
-	int ALUOp0;
-	int jump;
-	public void set_controler(String instruction){
-		//R-type
-		if (instruction.equals("000000")){
-			regDst = 1;
-			ALUSrc = 0;
-			MemToReg = 0;
-			RegWrite = 1;
-			MemRead = 0;
-			MemWrite = 0;
-			branch = 0;
-			ALUOp1 = 1;
-			ALUOp0 = 0;
-			jump = 0;
-		}
-		//lw
-		if (instruction.equals("100011")) {
-			regDst = 0;
-			ALUSrc = 1;
-			MemToReg = 1;
-			RegWrite = 1;
-			MemRead = 1;
-			MemWrite = 0;
-			branch = 0;
-			ALUOp1 = 0;
-			ALUOp0 = 0;
-			jump = 0;
-		}
-		//sw
-		if (instruction.equals("101011")) {
-			regDst = 0;
-			ALUSrc = 1;
-			MemToReg = 1;
-			RegWrite = 0;
-			MemRead = 0;
-			MemWrite = 1;
-			branch = 0;
-			ALUOp1 = 0;
-			ALUOp0 = 0;
-			jump = 0;
-		}
-		//sh
-		if (instruction.equals("101001")) {
-			regDst = 0;
-			ALUSrc = 1;
-			MemToReg = 1;
-			RegWrite = 0;
-			MemRead = 0;
-			MemWrite = 1;
-			branch = 0;
-			ALUOp1 = 0;
-			ALUOp0 = 0;
-			jump = 0;
-		}
-		//sb
-		if (instruction.equals("101000")) {
-			regDst = 0;
-			ALUSrc = 1;
-			MemToReg = 1;
-			RegWrite = 0;
-			MemRead = 0;
-			MemWrite = 1;
-			branch = 0;
-			ALUOp1 = 0;
-			ALUOp0 = 0;
-			jump = 0;
-		}
-		//beq
-		if (instruction.equals("000100")) {
-			regDst = 0;
-			ALUSrc = 0;
-			MemToReg = 0;
-			RegWrite = 0;
-			MemRead = 0;
-			MemWrite = 0;
-			branch = 1;
-			ALUOp1 = 0;
-			ALUOp0 = 1;
-			jump = 0;
-		}
-		//j
-		if (instruction.equals("000010")) {
-			regDst = 0;
-			ALUSrc = 0;
-			MemToReg = 0;
-			RegWrite = 0;
-			MemRead = 0;
-			MemWrite = 0;
-			branch = 1;
-			ALUOp1 = 0;
-			ALUOp0 = 1;
-			jump = 1;
-		}
-	}
+  
+  int regDst;
+  int ALUSrc;
+  int MemToReg;
+  int RegWrite;
+  int MemRead;
+  int MemWrite;
+  int branch;
+  int ALUOp1;
+  int ALUOp0;
+  int jump;
+
+  public void set_controler(String instruction){
+    
+    //R-type
+    if (instruction.equals("000000")){
+      regDst = 1;
+      ALUSrc = 0;
+      MemToReg = 0;
+      RegWrite = 1;
+      MemRead = 0;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 1;
+      ALUOp0 = 0;
+      jump = 0;
+    }
+    
+    //lw
+    if (instruction.equals("100011")) {
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 1;
+      RegWrite = 1;
+      MemRead = 1;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 0;
+      ALUOp0 = 0;
+      jump = 0;
+    }
+    
+    //lh
+    if (instruction.equals("100001")) {
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 1;
+      RegWrite = 1;
+      MemRead = 1;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 0;
+      ALUOp0 = 0;
+    }
+    
+    //sw
+    if (instruction.equals("101011")) {
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 1;
+      RegWrite = 0;
+      MemRead = 0;
+      MemWrite = 1;
+      branch = 0;
+      ALUOp1 = 0;
+      ALUOp0 = 0;
+      jump = 0;
+    }
+    
+    //sh
+    if (instruction.equals("101001")) {
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 1;
+      RegWrite = 0;
+      MemRead = 0;
+      MemWrite = 1;
+      branch = 0;
+      ALUOp1 = 0;
+      ALUOp0 = 0;
+      jump = 0;
+    }
+    
+    //sb
+    if (instruction.equals("101000")) {
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 1;
+      RegWrite = 0;
+      MemRead = 0;
+      MemWrite = 1;
+      branch = 0;
+      ALUOp1 = 0;
+      ALUOp0 = 0;
+      jump = 0;
+    }
+    
+    //addi
+    if (instruction.equals("001000")){
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 0;
+      RegWrite = 1;
+      MemRead = 0;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 1;
+      ALUOp0 = 0;
+    }
+    
+    //ori
+    if (instruction.equals("001101")){
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 0;
+      RegWrite = 1;
+      MemRead = 0;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 1;
+      ALUOp0 = 0;
+    }
+    
+    //andi
+    if (instruction.equals("001100")){
+      regDst = 0;
+      ALUSrc = 1;
+      MemToReg = 0;
+      RegWrite = 1;
+      MemRead = 0;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 1;
+      ALUOp0 = 0;
+    }
+    
+    //beq
+    if (instruction.equals("000100")) {
+      regDst = 0;
+      ALUSrc = 0;
+      MemToReg = 0;
+      RegWrite = 0;
+      MemRead = 0;
+      MemWrite = 0;
+      branch = 1;
+      ALUOp1 = 0;
+      ALUOp0 = 1;
+      jump = 0;
+    }
+    //j
+    if (instruction.equals("000010")) {
+      regDst = 0;
+      ALUSrc = 0;
+      MemToReg = 0;
+      RegWrite = 0;
+      MemRead = 0;
+      MemWrite = 0;
+      branch = 0;
+      ALUOp1 = 0;
+      ALUOp0 = 0;
+      jump = 1;
+    }
+  }
 }
